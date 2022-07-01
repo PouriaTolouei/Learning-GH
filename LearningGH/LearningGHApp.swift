de6fd2a214788bb413e475f626ce5c2b3d8a8c07
Pouria Tolouei <pouria81.t@gmail.com>
2022-07-01 15:10:25 +0700

    Initial Commit

---
+++ LearningGH/LearningGHApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningGHApp.swift
+//  LearningGH
+//
+//  Created by Pouria Tolouei on 01/07/2022.
+//
+
+import SwiftUI
+
+@main
+struct LearningGHApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
